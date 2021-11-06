4e862fbdd3b0922cb4ac8c412c825e2ca8543e05
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-07 02:14:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pyrogram import Client as Bot
 
 from callsmusic import run
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from handlers import __version__
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
@@ -17,7 +18,7 @@ bot = Bot(
     plugins=dict(root="handlers"),
 )
 
-print("[INFO]: Rocks Music Project !")
+print(f"[INFO]: ROCKS MUSIC BY ASAD v{__version__} STARTED !")
 
 bot.start()
 run()
