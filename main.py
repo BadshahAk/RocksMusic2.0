ac0d9d9455b7406ff9034fed8782cbd7236fa9e4
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 19:33:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,11 @@
 
 
 
-import requests
+
 from pyrogram import Client as Bot
 
 from callsmusic import run
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-
-response = requests.get(BG_IMAGE)
-with open("./etc/foreground.png", "wb") as file:
-    file.write(response.content)
+from config import API_ID, API_HASH, BOT_TOKEN, SUDO_USERS
 
 
 bot = Bot(
@@ -22,10 +18,10 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="handlers"),
+    plugins=dict(root="handlers")
 )
 
-print("[INFO]: Rocks Music Project !")
+print(f"[INFO]: ROCKS MUSIC VERSION 2.0 STARTED !")
 
 bot.start()
 run()
