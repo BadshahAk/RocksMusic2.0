eb10245f5a5c2777948ac0c6342d8b8b4b2fe4f9
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 18:12:15 +0500

    Update database.py

--- helpers/database.py
+++ helpers/database.py
@@ -1,12 +1,18 @@
-import datetime
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+
+
+import datetime
 import motor.motor_asyncio
+
+from config import DATABASE_URL, BOT_USERNAME
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-from config import BOT_USERNAME, DATABASE_URL
-
-
 class Database:
+
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
@@ -20,8 +26,8 @@ class Database:
                 is_banned=False,
                 ban_duration=0,
                 banned_on=datetime.date.max.isoformat(),
-                ban_reason="",
-            ),
+                ban_reason=''
+            )
         )
 
     async def add_user(self, id):
@@ -29,52 +35,56 @@ class Database:
         await self.col.insert_one(user)
 
     async def is_user_exist(self, id):
-        user = await self.col.find_one({"id": int(id)})
-        return bool(user)
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
 
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
 
     async def get_all_users(self):
-        return self.col.find({})
+        all_users = self.col.find({})
+        return all_users
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({"id": int(user_id)})
+        await self.col.delete_many({'id': int(user_id)})
 
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
             ban_duration=0,
             banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
+            ban_reason=''
         )
-        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
 
     async def ban_user(self, user_id, ban_duration, ban_reason):
         ban_status = dict(
             is_banned=True,
             ban_duration=ban_duration,
             banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason,
+            ban_reason=ban_reason
         )
-        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
 
     async def get_ban_status(self, id):
         default = dict(
             is_banned=False,
             ban_duration=0,
             banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
+            ban_reason=''
         )
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("ban_status", default)
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('ban_status', default)
 
     async def get_all_banned_users(self):
-        return self.col.find({"ban_status.is_banned": True})
+        banned_users = self.col.find({'ban_status.is_banned': True})
+        return banned_users
 
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
 
 # Database
 db = Database(DATABASE_URL, BOT_USERNAME)
+# Database for Anti-Cmd (I know what you are thinking about! but IDC)
 mongo_db_lmao = MongoClient(DATABASE_URL)
 dcmdb = mongo_db_lmao.handlers
