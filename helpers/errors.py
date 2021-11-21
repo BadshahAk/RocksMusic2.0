cc9025f275f4f8bd898cc7eeb2a8916ebade56b4
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 18:19:10 +0500

    Update errors.py

--- helpers/errors.py
+++ helpers/errors.py
@@ -1,6 +1,15 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
+
 class DurationLimitError(Exception):
     pass
 
 
 class FFmpegReturnCodeError(Exception):
     pass
+
+
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
