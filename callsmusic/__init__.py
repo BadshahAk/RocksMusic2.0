679933102eb932b07085983c751e6e8176d59dc2
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 17:50:56 +0500

    Update __init__.py

--- callsmusic/__init__.py
+++ callsmusic/__init__.py
@@ -1,2 +1,12 @@
-from . import queues
-from .callsmusic import pytgcalls, run
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
+
+
+
+from pyrogram import Client
+import config
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
+
+
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
