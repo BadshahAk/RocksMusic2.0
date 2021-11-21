f3b884781d44a7cf43756d58bc9af931c2e4363c
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 17:38:36 +0500

    Update str.py

--- str.py
+++ str.py
@@ -1,3 +1,8 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
 import asyncio
 
 from pyrogram import Client
