b04a2a4e2d3241c92cbdb86f4e8dcb8bab4ad71e
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 17:50:05 +0500

    Update admins.py

--- cache/admins.py
+++ cache/admins.py
@@ -1,6 +1,11 @@
-from typing import Dict, List
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
-from config import admins
+
+from typing import List, Dict, Union
+
 
 admins: Dict[int, List[int]] = {}
 
@@ -9,7 +14,11 @@ def set(chat_id: int, admins_: List[int]):
     admins[chat_id] = admins_
 
 
-def get(chat_id: int) -> List[int]:
+def get(chat_id: int) -> Union[List[int], bool]:
     if chat_id in admins:
         return admins[chat_id]
-    return []
+
+    return False
+
+
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
