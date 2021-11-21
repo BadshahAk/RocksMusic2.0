f120e5d464b839020b35f84bb02b6f17a926dfc0
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-21 17:49:14 +0500

    Update __init__.py

--- cache/__init__.py
+++ cache/__init__.py
@@ -1,3 +1,13 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
+
+
 from cache.admins import admins, get, set
 
 __all__ = ["admins", "get", "set"]
+
+
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
